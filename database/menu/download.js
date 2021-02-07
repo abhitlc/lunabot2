446a3c5757b97690227887d5d5de40b8ce80a1da
Ramlan666 <77728778+Ramlan666@users.noreply.github.com>
2021-02-07 15:10:31 +0700

    Add files via upload

---
+++ database/menu/download.js
@@ -0,0 +1,23 @@
+const download = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
+	return `🔰 -----[ *DOWNLOAD MENU* ]----- 🔰
+Hallo, ${pushname} 👋
+Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃╭───────────────────
+┃│➸ NAMA : ${pushname}
+┃│➸ LEVEL : ${getLevelingLevel(sender)}
+┃│➸ USER ${botName} : ${_registered.length}
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛
+Berikut adalah fitur yang ada pada bot ini!✨
+Jika tidak paham ketik *${prefix}bingungcok*
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃╭───────────────────
+┃│➸ *${prefix}play*
+┃│➸ *${prefix}yutubdl*
+┃│➸ *${prefix}tiktod*
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛
+🔰 -----[ *POWERED BY ${ownerName}* ]----- 🔰`
+}
+exports.download = download
---
+++ database/menu/sound.js
@@ -0,0 +1,29 @@
+const sound = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
+	return `🔰 -----[ *SOUND MENU* ]----- 🔰
+Hallo, ${pushname} 👋
+Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃╭───────────────────
+┃│➸ NAMA : ${pushname}
+┃│➸ LEVEL : ${getLevelingLevel(sender)}
+┃│➸ USER ${botName} : ${_registered.length}
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛
+Berikut adalah fitur yang ada pada bot ini!✨
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃╭───────────────────
+┃│➸ *${prefix}iri*
+┃│➸ *${prefix}pale*
+┃│➸ *${prefix}sound*
+┃│➸ *${prefix}sound1*
+┃│➸ *${prefix}sound2*
+┃│➸ *${prefix}sound3*
+┃│➸ *${prefix}sound4*
+┃│➸ *${prefix}sound5*
+┃│➸ *${prefix}sound6*
+┃│➸ *${prefix}sound7*
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛
+🔰 -----[ *POWERED BY ${ownerName}* ]----- 🔰`
+}
+exports.sound = sound
