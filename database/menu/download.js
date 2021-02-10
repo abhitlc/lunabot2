8d90217e6849549eb9ebe0e6273fe354bc48f5da
abhitlc <77800645+abhitlc@users.noreply.github.com>
2021-02-10 14:39:49 +0530

    Update download.js

--- database/menu/download.js
+++ database/menu/download.js
@@ -1,7 +1,7 @@
 const download = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
 	return `🔰 -----[ *DOWNLOAD MENU* ]----- 🔰
 Hallo, ${pushname} 👋
-Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
+Have a nice day for users, ${sender.split("@")[0]}
 ┏━━━━━━━━━━━━━━━━━━━━┓
 ┃╭───────────────────
 ┃│➸ NAMA : ${pushname}
@@ -9,8 +9,8 @@ Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
 ┃│➸ USER ${botName} : ${_registered.length}
 ┃╰───────────────────
 ┗━━━━━━━━━━━━━━━━━━━━┛
-Berikut adalah fitur yang ada pada bot ini!✨
-Jika tidak paham ketik *${prefix}bingungcok*
+The following are the features of this bot!✨
+TYPE *${prefix}command*
 ┏━━━━━━━━━━━━━━━━━━━━┓
 ┃╭───────────────────
 ┃│➸ *${prefix}play*
