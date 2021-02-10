8b460c6ead72b76d98369ab8213130c2fd87f259
abhitlc <77800645+abhitlc@users.noreply.github.com>
2021-02-10 14:49:39 +0530

    Update owb.js

--- database/menu/owb.js
+++ database/menu/owb.js
@@ -4,7 +4,7 @@ Hallo, ${pushname} 👋
 Have a nice day for users, ${sender.split("@")[0]}
 ┏━━━━━━━━━━━━━━━━━━━━┓
 ┃╭───────────────────
-┃│➸ NAMA : ${pushname}
+┃│➸ NAME : ${pushname}
 ┃│➸ LEVEL : ${getLevelingLevel(sender)}
 ┃│➸ USER ${botName} : ${_registered.length}
 ┃╰───────────────────
