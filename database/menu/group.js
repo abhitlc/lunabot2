5583a2a77c322d230008635e6632e1539e54e6db
abhitlc <77800645+abhitlc@users.noreply.github.com>
2021-02-10 14:44:11 +0530

    Update group.js

--- database/menu/group.js
+++ database/menu/group.js
@@ -1,7 +1,7 @@
 const groupm = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
 	return `🔰 -----[ *GROUP MENU* ]----- 🔰
 Hallo, ${pushname} 👋
-Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
+Have a nice day for users, ${sender.split("@")[0]}
 ┏━━━━━━━━━━━━━━━━━━━━┓
 ┃╭───────────────────
 ┃│➸ NAMA : ${pushname}
@@ -9,8 +9,8 @@ Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
 ┃│➸ USER ${botName} : ${_registered.length}
 ┃╰───────────────────
 ┗━━━━━━━━━━━━━━━━━━━━┛
-Berikut adalah fitur yang ada pada bot ini!✨
-Jika tidak paham ketik *${prefix}bingungcok*
+The following are the features of this bot!✨
+If you don't understand, type *${prefix}command*
 ┏━━━━━━━━━━━━━━━━━━━━┓
 ┃╭───────────────────
 ┃│➸ *${prefix}welcome* ⟪ 1/0 ⟫
